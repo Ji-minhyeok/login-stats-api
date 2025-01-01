8722c5614faa1db7cf09cecc38bfacd9aa016639
Ji-minhyeok <gmh8257@gmail.com>
2025-01-01 23:53:53 +0900

    Initial commit

---
+++ src/main/java/com/loginstats/login_stats_api/LoginStatsApiApplication.java
@@ -0,0 +1,13 @@
+package com.loginstats.login_stats_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginStatsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginStatsApiApplication.class, args);
+	}
+
+}
